985456d897641a20a5391184da9b93d9868cfa19
Unknown <mak.bekmamat@gmail.com>
2018-02-05 04:17:48 +0500

    get token from localStorage

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,10 +1,15 @@
-const INITIAL_STATE = { token: null, error: false, loading: false };
+const INITIAL_STATE = {
+  token: localStorage.getItem('token'),
+  error: false,
+  loading: false,
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch(action.type) {
     case 'SIGNIN_REQUEST':  
       return {...state, loading: true };
     case 'SIGNIN_SUCCESS':
+      localStorage.setItem('token', action.payload);
       return {token: action.payload, error: false, loading: false }
     case 'SIGNIN_FAILURE':
       return { token: null, error: action.payload, loading: false }

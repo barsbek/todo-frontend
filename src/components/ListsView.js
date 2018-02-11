5d07ed73e64ed88db5e05586b301964d3fcbe0ad
barsbek <mak.bekmamat@gmail.com>
2018-02-11 16:44:33 +0500

    style lists component

--- src/components/ListsView.js
+++ src/components/ListsView.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CSSModules from 'react-css-modules';
 
 import List from '../containers/List';
+import styles from '../styles/components/Lists.css';
 
 const ListsView = ({
   lists,
   addNewList,
   withUnsaved,
-}) => [
-  <div key="lists">
-    {lists.map(list => (
-      <List key={list.id} {...list} />
-    ))}
-  </div>,
-  (!withUnsaved &&
-    <button key="new-list" onClick={addNewList}>+</button>
-  )
-]
+}) => (
+  <div styleName="container">
+    <div key="lists" styleName="lists">
+      {lists.map(list => <List key={list.id} {...list} />)}
+    </div>
+    {!withUnsaved &&
+      <button
+        key="new-list"
+        styleName="new-list-button"
+        onClick={addNewList}>
+          +
+      </button>
+    }
+  </div>
+)
 
 ListsView.propTypes = {
   lists: PropTypes.array.isRequired,
   addNewList: PropTypes.func.isRequired,
 }
 
-export default ListsView;
+export default CSSModules(ListsView, styles);

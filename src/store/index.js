af440597e1a260a1b2d74842278545124af996cf
barsbek <mak.bekmamat@gmail.com>
2018-02-11 07:51:42 +0500

    configure store in a single file

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,24 @@
+import { compose, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from '../reducers';
+import api from '../middlewares/api';
+import DevTools from '../containers/DevTools';
+
+const middlewares = [thunk, api];
+let storeCompose;
 if( process.env.NODE_ENV === 'production') {
-  module.exports = require('./prod');
+  storeCompose = compose(
+    applyMiddleware(thunk, api),
+  )
 } else {
-  module.exports = require('./dev');
+  storeCompose = compose(
+    applyMiddleware(thunk, api),
+    DevTools.instrument()
+  )
 }
+
+export default createStore(
+  reducers,
+  storeCompose,
+);

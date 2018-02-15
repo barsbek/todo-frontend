0a46bad25a990de3f4c81173b2640b3c42a68038
barsbek <mak.bekmamat@gmail.com>
2018-02-16 03:03:25 +0500

    remove spare

--- src/actions/design.js
+++ src/actions/design.js
@@ -1,11 +1,3 @@
-export const showProfileMenu = () => ({
-  type: 'SHOW_PROFILE_MENU'
-})
-
-export const hideProfileMenu = () => ({
-  type: 'HIDE_PROFILE_MENU'
-})
-
 export const toggleProfileMenu = () => ({
   type: 'TOGGLE_PROFILE_MENU'
 })
--- src/reducers/design.js
+++ src/reducers/design.js
@@ -4,10 +4,6 @@ const INITIAL_STATE = {
 
 export default function(state=INITIAL_STATE, action) {
   switch(action.type) {
-  case 'SHOW_PROFILE_MENU':
-    return { ...state, menuOpen: true }
-  case 'HIDE_PROFILE_MENU':
-    return { ...state, menuOpen: false }
   case 'TOGGLE_PROFILE_MENU':
     const { menuOpen } = state;
     return { ...state, menuOpen: !menuOpen }

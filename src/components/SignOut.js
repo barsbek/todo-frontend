055c7be7eb52a53bd8648f64745f5c3d95b0fedb
barsbek <mak.bekmamat@gmail.com>
2018-02-19 09:54:50 +0500

    correct path

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
-import { signOut } from '../actions';
+import { signOut } from '../redux-app/actions';
 
 class SignOut extends Component {
   componentWillMount() {
